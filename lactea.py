5bc94579730065550b66619024a230741e60ff30
Jennifer de Souza <jdszza.devs@gmail.com>
2024-03-11 21:30:23 -0300

    Add files via upload

---
+++ lactea.py
@@ -0,0 +1,38 @@
+# n = 3.14159
+# raio = float(input())
+# area = n * raio**2
+# print(f"A={area:.4f}")
+
+# A = int(input())
+# B = int(input())
+# SOMA = A + B
+# print(f"SOMA = {SOMA}")
+
+# A = int(input())
+# B = int(input())
+# PROD = A * B
+# print(f"PROD = {PROD}")
+
+# A = float(input())
+# B = float(input())
+# C = float(input())
+# MEDIA = (A * 0.2) + (B * 0.3) + (C * 0.5)
+# print(f"MEDIA = {MEDIA:.1f}")
+
+# A = int(input())
+# B = int(input())
+# C = int(input())
+# D = int(input())
+# DIFERENCA = (A * B - C * D)
+# print(f"DIFERENCA = {DIFERENCA}")
+
+# nome = str(input("primeiro nome do vendedor"))
+# salario = float(input())
+# total_vendas = float(input())
+# TOTAL = salario + (total_vendas * 0.15)
+# print(f"TOTAL = R$ {TOTAL:.2f}")
+
+X = int(input())
+Y = float(input())
+consumo = X // Y
+print(f"{consumo:.3f} km/l")
